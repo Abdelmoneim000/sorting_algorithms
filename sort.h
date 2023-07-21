9320c6b8551e5a787b729df9b61e3d7653cd0183
Sokayna <sokayna.bennasseur@gmail.com>
2023-07-21 10:13:11 -0400

    Fix: typo

--- sort.h
+++ sort.h
@@ -26,6 +26,4 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 int lomuto_partition(int *array, size_t size, int low, int high);
 void quick_sort_recursion(int *array, size_t size, int low, int high);
-#endif
-
 #endif /* SORT_H */
